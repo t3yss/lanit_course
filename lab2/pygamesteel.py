7c42cebd21f65afa64762c74c19ee4092347ed7a
Максим Зенков <max.zenkov@yahoo.com>
2026-04-30 15:06:15 +0300

    lab2 start

---
+++ lab2/pygamesteel.py
@@ -0,0 +1,26 @@
+import pygame
+pygame.init()
+
+# Устанавливаем размеры окна
+screen_width = 800
+screen_height = 600
+window_size = (screen_width, screen_height)
+pygame.display.set_mode(window_size) # Создаем окно
+
+# Задаем цвет фона
+bg_color = (255, 255, 255)
+pygame.draw.rect(screen, bg_color, [0, 0, screen_width, screen_height], 1)
+
+# Выводим текст на экран
+font = pygame.font.SysFont(None, 75)
+text = font.render("Hello appsec world*", True, (0, 255, 0))
+text_rect = text.get_rect()
+text_rect.center = (400, 300)
+screen.blit(text, text_rect)
+
+while True:
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            pygame.quit()
+            quit()
+pygame.display.flip() # Обновляем экран
